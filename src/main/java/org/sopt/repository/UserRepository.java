03e328fa02ed3085c3004cc9f777462d7bec4579
kimhoeyun <clnvrd@suwon.ac.kr>
2026-04-29 15:02:37 +0900

    feat: User 엔티티 생성 및 Post와 연관관계 설정

---
+++ src/main/java/org/sopt/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.sopt.repository;
+
+import org.sopt.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
