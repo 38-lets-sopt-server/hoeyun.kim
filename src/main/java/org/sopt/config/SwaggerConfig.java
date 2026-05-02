4562f1fbd3be1884264c05b9db0b69e08abdeed5
kimhoeyun <clnvrd@suwon.ac.kr>
2026-05-02 16:50:49 +0900

    docs: Swagger API 문서 설정 추가

---
+++ src/main/java/org/sopt/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package org.sopt.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SOPT_에브리타임")
+                        .description("")
+                        .version("v1"));
+    }
+}
